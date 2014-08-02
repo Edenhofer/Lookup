6965bb1fea3131c1407e74c6bf6bebc2725bb3db
Gordian Edenhofer <gordian.edenhofer@yahoo.de>
2014-08-02 13:23:27 +0200

    Work In Progress (WIP)

--- Lookup/google_translate.js
+++ Lookup/google_translate.js
@@ -37,6 +37,7 @@ function search() {
 	}
 
 	var request = makeHttpObject();
+	// request.setRequestHeader('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1985.125 Safari/537.36');
 	request.open("GET", url, true);
 	request.send(null);
 	request.onreadystatechange = function() {
--- Lookup/options.js
+++ Lookup/options.js
@@ -1,46 +1,36 @@
+// Until now the function is not calles
+function style_display() {
+	var language = document.getElementById("language").value;
+	var grounding = document.getElementById("grounding").value;
+	
+	if (language != "de") document.getElementById("ger_d").style.display = "none";
+	else document.getElementById("ger_d").style.display = "inline";
+}
+
 // Saves options to chrome.storage
 function save_options() {
 	var language = document.getElementById("language").value;
 	var grounding = document.getElementById("grounding").value;
-	chrome.storage.sync.set({
-		language: language,
-		grounding: groundning
-		}, function() {
-			// Update status to let user know options were saved.
-			var status = document.getElementById("status");
-			status.innerHTML = "Options saved.";
-			setTimeout(function() {
-				status.textContent = "";
-			}, 750);
-		}
-	);
-}
-function default_options() {
-	var language = document.getElementById("language").value;
-	var grounding = document.getElementById("grounding").value;
-	chrome.storage.sync.set({
-		language: language,
-		grounding: groundning
-		}, function() {
-			// Update status to let user know options were saved.
-			var status = document.getElementById("status");
-			status.innerHTML = "Options saved.";
-			setTimeout(function() {
-				status.textContent = "";
-			}, 750);
-		}
-	);
-}
-// Restore selected preferences
-function restore_options() {
-	chrome.storage.sync.get({
-		language: "de",
-		grounding: "wikipedia"
-	}, function(items) {
-		document.getElementById("language").value = items.language;
-		document.getElementById("grounding").value = items.groundning;
+
+	chrome.storage.sync.set({"language": language, "groundning": grounding}, function() {
+		//if (runtime.lastError) alert("Unexpected Error");
+		// This does not work yet!
+		document.getElementById("status").innerHTML = "Settings saved";
+		document.getElementById("status").style.display = "inline";
+		
+		chrome.storage.sync.get(("language", "grounding"), function(language, grounding) {
+			alert(language);
+		});
 	});
 }
-document.addEventListener("DOMContentLoaded", restore_options);
-document.getElementById("save").addEventListener("submit", save_options);
-document.getElementById("restore").addEventListener("submit", default_options);
+
+// Setting default options
+function default_options() {
+	var language = document.getElementById("language").value;
+	var grounding = document.getElementById("grounding").value;	
+}
+
+window.addEventListener("load", function(evt) {
+	document.getElementById("save").addEventListener("submit", save_options);
+	document.getElementById("save_default").addEventListener("submit", default_options);
+});
