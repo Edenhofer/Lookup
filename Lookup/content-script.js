a6b43d72224a863e65d6315f7034ecb321e5c671
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2015-08-09 12:52:15 +0200

    Proper content-script and infobox without background-page
    The infobox.html page is now a division which is injected into the
    page with a double click and removed with a single click. For this
    to work the manifest file was altered. Furthermore the background
    page and therefore the message passing system has been removed
    since there is no obvious use for it.

--- Lookup/content-script.js
+++ Lookup/content-script.js
@@ -1,8 +1,35 @@
+// Custom JSLint configurtations
+// Allow the use ECMAScript 6 specific syntax, e.g. const
+// jshint esnext: true
+
+// Setting up some constants
+const max_output_length = 540;
+const max_last_queries = 10;
+const xmlhttp_timeout = 5000; // in milliseconds
+const pre_html_id = "lookup-infobox-";
+
 document.body.addEventListener('dblclick', function () {
     var query = window.getSelection().toString();
-    if (query === "" || query == " " || query == ".") return -1;
+    if ((query.length == 1 && query.match(/[\W\d]/)) || query === "") return -1;
 
-    chrome.runtime.sendMessage({message: query}, function (response) {
-        console.log("[DBCLICK]: \"" + window.getSelection().toString() + "\" - sending message to extension - waiting for a response - " + response.back);
-    });
+    // Inject the infobox.html division into the site
+    var infobox_url = chrome.extension.getURL("infobox.html");
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var object = document.createElement("div");
+            object.id = pre_html_id + "main-container";
+            object.style.display = "none"; // Hiding the infobox for now. - TODO
+            object.innerHTML = xmlhttp.responseText;
+            document.body.appendChild(object);
+        }
+    };
+    xmlhttp.open("GET", infobox_url, true);
+    xmlhttp.send();
+});
+
+document.body.addEventListener('click', function () {
+    if (document.getElementById(pre_html_id + "main-container") !== null) {
+        document.body.removeChild(document.getElementById(pre_html_id + "main-container"));
+    }
 });
